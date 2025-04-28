62e68c616890826b35d16f59f628aba61abbd275
Nguyen, Derek <duy.nguyen.2005.college@gmail.com>
2025-04-28 04:30:06 -0400

    Saved alerts test

--- weather/urls.py
+++ weather/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('alerts/', views.alerts_view, name='weather.alerts'),
     path('alerts/add/', views.add_alert, name='weather.add_alert'),
     path('alerts/delete/<int:alert_id>/', views.delete_alert, name='weather.delete_alert'),
-    path('alerts/dismiss/<int:alert_id>/', views.dismiss_alert, name='weather.dismiss_alert')
+    path('alerts/dismiss/<int:alert_id>/', views.dismiss_alert, name='weather.dismiss_alert'),
+    path("alerts/save/", views.save_alert, name="weather.save_alert"),
 ]
